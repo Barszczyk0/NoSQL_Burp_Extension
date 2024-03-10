ca89f32f8e6984a937d3fca7979d0e554f44c386
Adam <mielewskiadam@gmail.com>
2024-03-10 13:59:21 +0100

    test

--- src/main/java/BurpExtention.java
+++ src/main/java/BurpExtention.java
@@ -6,6 +6,6 @@ public class BurpExtention implements BurpExtension {
     @Override
     public void initialize(MontoyaApi montoyaApi) {
         montoyaApi.extension().setName("NoSQL_Burp_Extension");
-        montoyaApi.logging().logToOutput("Hello World");
+        montoyaApi.logging().logToOutput("Hello World!");
     }
 }
