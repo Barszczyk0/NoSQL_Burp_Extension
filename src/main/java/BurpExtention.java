206c16d1ff3f4bf7cee25cc5914b5f9d440676de
Barszczyk <93995920+Barszczyk0@users.noreply.github.com>
2024-03-10 13:48:51 +0100

    First commit

---
+++ src/main/java/BurpExtention.java
@@ -0,0 +1,11 @@
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+
+public class BurpExtention implements BurpExtension {
+
+    @Override
+    public void initialize(MontoyaApi montoyaApi) {
+        montoyaApi.extension().setName("NoSQL_Burp_Extension");
+        montoyaApi.logging().logToOutput("Hello World");
+    }
+}
